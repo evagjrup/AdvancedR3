72b25fbee400be6a885bdfc0a81acb4c9be8bddd
Eva Marie Gjørup <evagjoerup@clin.au.dk>
2023-11-09 10:57:18 +0100

    Made a new function with ggplot

--- R/functions.R
+++ R/functions.R
@@ -9,3 +9,16 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Plot distributions
+#'
+#' @param data lipidomics data
+#'
+#' @return metabolite distribution plot
+plot_distributions <- function(data) {
+    metabolite_distribution_plot <- ggplot2::ggplot(data,
+                                                    ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+    return(metabolite_distribution_plot)
+}
