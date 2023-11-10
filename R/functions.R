eb797530c26cc0b5028e3f853dec596653ffc002
Eva Marie Gjørup <evagjoerup@clin.au.dk>
2023-11-10 11:13:52 +0100

    Finished session 8

--- R/functions.R
+++ R/functions.R
@@ -169,13 +169,13 @@ calculate_estimates <- function(data) {
 #' @return ggplot / forest plot
 #'
 plot_estimates <- function(results) {
-    model_estimates %>%
-        ggplot2::ggplot(ggplot2::aes(
-            x = estimate,
-            y = metabolite,
-            xmin = estimate - std.error,
-            xmax = estimate + std.error
-        )) +
-        ggplot2::geom_pointrange() +
-        ggplot2::coord_fixed(xlim = c(0, 5))
+  results %>%
+    ggplot2::ggplot(ggplot2::aes(
+      x = estimate,
+      y = metabolite,
+      xmin = estimate - std.error,
+      xmax = estimate + std.error
+    )) +
+    ggplot2::geom_pointrange() +
+    ggplot2::coord_fixed(xlim = c(0, 5))
 }
